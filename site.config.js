ad1c6a78f55d5b3b45d759d38b2929b4cbd4d6bc
bgy8023 <74339601+bgy8023@users.noreply.github.com>
2021-08-09 04:10:55 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -19,7 +19,7 @@ module.exports = {
   // social usernames (optional)
   twitter: 'transitive_bs',
   github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  wechat: 'bgy8023',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
